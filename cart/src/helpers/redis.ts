7099b562605c2e8b774e0eccf81f4ccdd921703c
Barış Yesari <105274476+Yesarib@users.noreply.github.com>
2024-02-07 16:16:15 +0300

    docker-compose

--- cart/src/helpers/redis.ts
+++ cart/src/helpers/redis.ts
@@ -1,10 +1,11 @@
 import { createClient } from 'redis'
 
 const client = createClient({
-    socket:{
-        port: 6379,
-        host: "127.0.0.1"
-    }
+    // socket:{
+    //     port: 6379,
+    //     host: "127.0.0.1"
+    // },
+    url: 'redis://redis:6379'
 });
 
 (async () => { 
